98afb5cd01731d19083a229d8bb6debe3765867d
Marquez Moore <marquezmoore23@gmail.com>
2021-02-27 17:33:23 +0100

    Added .gitignore

--- server.js
+++ server.js
@@ -1,31 +1,34 @@
-let http = require('http');
-let url = require('url');
+let http = require('http'),
+  url = require('url'),
+  fs = require('fs');
 
 http.createServer((req, res) =>{
-  res.writeHead(200, {'contentType': 'text/plain'});
   let q = url.parse(req.url, true);
   filePath = '';
 
-  if(q.path.include('documentation')){//set filePath to requested path in current directory
+  if(q.pathname.includes('documentation')){//set filePath to requested path in current directory
     filePath = `${__dirname}/documentation.html`;
   }else{// set filePath to home page
     filePath = index.html;
   }
 
-// Read the contents of the form filePath and return/log the data
-  fs.readFile(filePath, 'utf-8', (err, data) => {
-    if(err) {
-      throw err;
-    }else{
-      console.log(data);
-    }
-  })
 //Update the log.txt file using the 'f'ile 's'ystem module's appendFile method, with the request url.
   fs.appendFile('log.txt', `Date: ${new Date()}:\n URL: ${req.url}\n\n`, (err,file) =>{
     if(err) throw err;
     console.log(`${file} Saved!`);
   })
   
+// Read the contents of the form filePath and return/log the data
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if(err) {
+      throw err;
+    }
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+    
+  })
 
 }).listen(8080);//Listen for http requests on port 8080
 
